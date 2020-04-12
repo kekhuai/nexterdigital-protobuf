17c217a980f55f1a8b78bd6b6235f396330e7f99
Natthaphon Phanit <4550615-kekhuay@users.noreply.gitlab.com>
2020-04-13 00:32:19 +0700

    revise pre-push script

--- pre_push.py
+++ pre_push.py
@@ -9,22 +9,27 @@ def build_all():
     repo_path = '/tmp/protolangs'
     if not os.path.exists(repo_path):
         os.makedirs(repo_path)
-
-    for root, dirs, files in os.walk('./'):
+    
+    root = os.getcwd()
+    dirs = [os.path.join(root, dir) for dir in os.listdir('.') if os.path.isdir(dir)]
+    for dir in dirs:
+        os.chdir(dir)
+        files = [f for f in os.listdir('.') if os.path.isfile(f)]
         if '.protolangs' in files:
-            os.chdir(root)
             with open('.protolangs', 'r') as file:
                 languages = [line.rstrip() for line in file]
                 for language in languages:
-                    repo_name = 'protobuf-%s-%s' % (os.path.basename(root), language)
+                    cwd = os.getcwd()
+                    repo_name = 'protobuf-%s-%s' % (os.path.basename(dir), language)
                     shutil.rmtree('%s/%s' % (repo_path, repo_name), ignore_errors=True)
                     os.system('git clone git@github.com:kekhuay/%s.git %s/%s' % (repo_name, repo_path, repo_name))
-                    os.system('docker container run --rm --mount type=bind,source=`pwd`,target=/defs namely/protoc-%s' % (language))
+                    os.system('docker container run --rm --mount type=bind,source=`pwd`,target=/defs namely/protoc-all -d ./ -o pb-%s -l %s' % (language, language))
                     os.system('cp -R pb-%s/* %s/%s/' % (language, repo_path, repo_name))
                     os.chdir('%s/%s' % (repo_path, repo_name))
                     os.system('git add .')
                     os.system('git commit -m "Auto Creation of Proto"')
                     os.system('git push -u origin master')
+                    os.chdir(dir)
 
 
 if '__main__' == __name__:
